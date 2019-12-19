a1c09d2968965bce1a5c70defd06ea5530217b40
donfo <aspzjfa@foxmail.com>
2019-12-19 17:49:23 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# wit-uni-cli
+
+## 安装
+```
+npm install wit-uni-cli --global
+or
+yarn global add wit-uni-cli
+```
+
+## 使用
+
+```
+$ wit-uni
+or
+$ wit-uni watch
+```
---
+++ index.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+let fs = require('fs')
+let shell = require('shelljs')
+let watch = require('node-watch')
+let dayjs = require('dayjs')
+
+let readFileJSON = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.log(err)
+        reject(err)
+      } else {
+        resolve(JSON.parse(data))
+      }
+    })
+  })
+}
+
+let init = async () => {
+  console.log(`处理中，请稍后...[${dayjs().format('YYYY-MM-DD hh:mm:ss')}]`)
+  let pagesDefault = await readFileJSON('./pages-default.json')
+  let files = shell.find('./pages/app/**/pages.json')
+  for (i = 0; i < files.length; i++) { 
+    let data = await readFileJSON(files[i])
+    pagesDefault.pages.push(...data.pages)
+  }
+  console.log(pagesDefault)
+  fs.writeFile('./pages.json', JSON.stringify(pagesDefault), (err) => {
+     if(err) throw err
+     console.log(`处理成功！[${dayjs().format('YYYY-MM-DD hh:mm:ss')}]\n`)
+  })
+}
+
+if (process.argv[2] === 'watch') {
+  watch(['./pages/app/', './pages-default.json'], { recursive: true, filter: /pages.json$/ }, () => {
+    init()
+  })
+}
+
+init()
